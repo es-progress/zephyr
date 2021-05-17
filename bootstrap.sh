0d51ebe68a8a07dc3aca15e9605d276155047054
Sandor Semsey <sandor@es-progress.hu>
2021-05-17 23:43:21 +0200

    bootstrap configs

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+# shellcheck shell=bash
+##################
+## ES-Ubuntu    ##
+##              ##
+## Bootstrapper ##
+##################
+
+# Import configs
+source "${ESUBUNTU_HOME}/ESubuntu.cfg"
+
+# Import shell library
+source "${ESUBUNTU_HOME}/bin/library.sh"
