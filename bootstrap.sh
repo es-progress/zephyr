e550720e2203b2ed844ce229351ec36551df9f6f
Sandor Semsey <sandor@es-progress.hu>
2022-08-26 00:38:35 +0200

    PATH_MOUNT

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,6 +21,8 @@ export ES_CUSTOMIZE_FILES="${PROJECT_ROOT}/files"
 
 # Customization profiles
 export PATH_PROFILES="${PROJECT_ROOT}/profiles"
+# Mount point for iso images
+export PATH_MOUNT="${PROJECT_ROOT}/bundling/mnt"
 # Extracted files from iso image
 export PATH_EXTRACT="${PROJECT_ROOT}/bundling/extract"
 
