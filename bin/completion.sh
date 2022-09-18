0e2d5bf7c97cea0f94a7d3e217f81e4ec797df58
Sandor Semsey <sandor@es-progress.hu>
2022-09-19 00:59:09 +0200

    esubuntuctl: completion

--- bin/completion.sh
+++ bin/completion.sh
@@ -1,27 +1,51 @@
 ## Bash completion for esubuntuctl
 ##################################
-_complete_esubuntuctl() {
-    local index="${COMP_CWORD}"
-    local cur="${COMP_WORDS[${index}]}"
-    local command=${COMP_WORDS[1]}
-    local commands="burn create help install partition post-install customize self-install"
+__esubuntuctl_complete() {
+    local cur words cword command commands profiles scripts
+    _init_completion
 
-    [[ "${index}" == 1 ]] && readarray -t COMPREPLY < <(compgen -W "${commands}" "${cur}")
+    command="${words[1]}"
+    commands=(burn create help install partition post-install customize self-install)
+    profiles=$(find -L "{{ INSTALL_DIR }}/profiles" -mindepth 1 -maxdepth 1 -type d -printf "%f\n")
+    scripts=$(find "{{ INSTALL_DIR }}/bin/customize" -type f -printf "%f\n")
 
-    if [[ "${index}" == 2 ]]; then
-        case "${command}" in
-            help) readarray -t COMPREPLY < <(compgen -W "${commands}" "${cur}") ;;
-            customize) readarray -t COMPREPLY < <(compgen -W "$(find -L "{{ INSTALL_DIR }}/profiles" -mindepth 1 -maxdepth 1 -type d -printf "%f\n")" "${cur}") ;;
-            create|burn|install) readarray -t COMPREPLY < <(compgen -f "${cur}") ;;
-        esac
-    fi
-
-    if [[ "${index}" == 3 ]]; then
-        case "${command}" in
-            burn) readarray -t COMPREPLY < <(compgen -f "${cur}") ;;
-            customize) readarray -t COMPREPLY < <(compgen -W "$(find "{{ INSTALL_DIR }}/bin/customize" -type f -printf "%f\n")" "${cur}") ;;
-        esac
-    fi
+    case "${cword}" in
+        1) __esubuntu_gen_word "${commands[*]}" ;;
+        2)
+            case "${command}" in
+                help) __esubuntu_gen_word "${commands[*]}" ;;
+                customize) __esubuntu_gen_word "${profiles}" ;;
+                create|burn|install) __esubuntu_gen_file ;;
+                *) ;;
+            esac
+            ;;
+        3)
+            case "${command}" in
+                burn) __esubuntu_gen_file ;;
+                customize) __esubuntu_gen_word "${scripts}" ;;
+                *) ;;
+            esac
+            ;;
+        *) ;;
+    esac
 }
 
-complete -F _complete_esubuntuctl -o filenames esubuntuctl
+## Generate completions from word list
+##
+## @param   $*  Word list
+######################################
+__esubuntu_gen_word() {
+    local result
+    result=$(compgen -W "${*}" "${cur}")
+    readarray -t COMPREPLY <<<"${result}"
+}
+
+## Generate completions from filenames
+######################################
+__esubuntu_gen_file() {
+    local result
+    result=$(compgen -f "${cur}")
+    readarray -t COMPREPLY <<<"${result}"
+}
+
+complete -F __esubuntuctl_complete -o filenames esubuntuctl
