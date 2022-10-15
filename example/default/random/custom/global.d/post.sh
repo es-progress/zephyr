0985953cba58b3818417ff625ce083f55db71bf4
Sandor Semsey <sandor@es-progress.hu>
2022-10-15 02:49:20 +0200

    example: random

---
+++ example/default/random/custom/global.d/post.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+##################
+## ES-Ubuntu    ##
+##              ##
+## Example      ##
+## Local script ##
+##################
+
+# Strict mode
+set -eufo pipefail
+IFS=$'\n\t'
+
+echo Running local script...
+pwd
+echo Finished
+
+exit 0
