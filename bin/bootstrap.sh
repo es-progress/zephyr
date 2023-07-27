0b289f82a74d7f8cf9a1fc60acc9ea86174a284b
Sandor Semsey <sandor@es-progress.hu>
2023-07-27 15:46:07 +0200

    zephyrctl/remix: make output ISO filename a parameter

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -14,8 +14,6 @@ PATH_SCRIPTS_CUSTOMIZE="${PROJECT_ROOT}/bin/customize"
 PATH_SCRIPTS_INSTALL="${PROJECT_ROOT}/bin/install"
 # Customization profiles
 PATH_PROFILES="${PROJECT_ROOT}/profiles"
-# ISO files
-PATH_ISO="${PROJECT_ROOT}/mixing/iso"
 # Mount point for iso images
 PATH_MOUNT="${PROJECT_ROOT}/mixing/mnt"
 # Extracted files from iso image
