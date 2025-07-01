47e8a5c7c2409912c265c7994e176997aeb67541
Sandor Semsey <sandor@es-progress.hu>
2025-07-01 02:22:05 +0200

    zephyrctl: remove deprecated format, partition commands

--- bin/completion.sh
+++ bin/completion.sh
@@ -6,10 +6,9 @@ __zephyrctl_complete() {
     _init_completion
 
     command="${words[1]}"
-    commands=(extract-grub burn remix help launch disk partition format post-install customize install uninstall)
+    commands=(extract-grub burn remix help launch disk post-install customize install uninstall)
     profiles=
     [[ -r "{{ INSTALL_DIR }}/profiles" ]] && profiles=$(find -L "{{ INSTALL_DIR }}/profiles" -mindepth 1 -maxdepth 1 -type d -printf "%f\n")
-    maps=
     scripts=$(find "{{ INSTALL_DIR }}/bin/customize" -mindepth 2 -type f -executable -printf "%f\n^%f\n")
     categories=$(find "{{ INSTALL_DIR }}/bin/customize" -mindepth 1 -maxdepth 1 -type d -printf "%f\n^%f\n")
 
@@ -19,7 +18,7 @@ __zephyrctl_complete() {
             case "${command}" in
                 help) __zephyr_gen_word "${commands[*]}" ;;
                 post-install) __zephyr_gen_word live install dev ;;
-                customize|partition|format) __zephyr_gen_word "${profiles}" ;;
+                customize) __zephyr_gen_word "${profiles}" ;;
                 extract-grub|remix|burn) __zephyr_gen_file ;;
                 disk) __zephyr_gen_word decrypt encrypt format info lvm partition zap ;;
                 *) ;;
@@ -30,10 +29,6 @@ __zephyrctl_complete() {
                 extract-grub|remix|burn) __zephyr_gen_file ;;
                 customize) __zephyr_gen_word "${scripts}" "${categories}" ;;
                 disk|post-install) __zephyr_gen_word "${profiles}" ;;
-                partition|format)
-                    [[ -r "{{ INSTALL_DIR }}/profiles" ]] && maps=$(find -L "{{ INSTALL_DIR }}/profiles" -type f -path "*${words[2]}/install/disk-maps*" -printf "%f\n")
-                    __zephyr_gen_word "${maps}"
-                    ;;
                 *) ;;
             esac
             ;;
@@ -41,11 +36,8 @@ __zephyrctl_complete() {
             case "${command}" in
                 customize) __zephyr_gen_word "${scripts}" "${categories}" ;;
                 disk|post-install)
+                    maps=
                     [[ -r "{{ INSTALL_DIR }}/profiles" ]] && maps=$(find -L "{{ INSTALL_DIR }}/profiles" -type f -path "*${words[3]}/install/disk-maps*" -printf "%f\n")
-                    __zephyr_gen_word "${maps}"
-                    ;;
-                partition|format)
-                    [[ -r "{{ INSTALL_DIR }}/profiles" ]] && maps=$(find -L "{{ INSTALL_DIR }}/profiles" -type f -path "*${words[2]}/install/disk-maps*" -printf "%f\n")
                     __zephyr_gen_word "${maps}"
                     ;;
                 *) ;;
